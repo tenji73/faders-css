a2a3d1cdef39fccbe80b520241a03f01a394bb3f
tom anyz <tom@appjigger.com>
2016-02-24 18:12:21 +0100

    delays added

--- faders.js
+++ faders.js
@@ -3,12 +3,29 @@ faders.js
 licensed under the MIT license - http://opensource.org/licenses/MIT
 copyright (c) 2015 tom anyz
 */
-$(window).scroll(function() {
-	$('[class^="fadeFrom"]').each(function()
+$(function () 
+{
+	var addFader = function(obj)
 	{
-		if ($(this).offset().top < $(window).scrollTop() + $(window).height() * .9 ) 
-		{
-			$(this).addClass("animation-end");
-		}
+		var preg = $(obj).attr('class').match(/fadeDelay-([0-9]*)/);
+		var delay = preg ? parseInt(preg[1]) : 10;
+		setTimeout(function(){  $(obj).addClass("animation-end"); } ,delay);
+	}
+
+	$('.fadeOnLoad').each(function()
+	{
+		addFader(this);	 
 	});
+
+	$(window).scroll(function() {
+		$('[class^="fadeFrom"]').each(function()
+		{
+			if ($(this).offset().top < $(window).scrollTop() + $(window).height() * .9 && !$(this).attr('class').match(/animation-end/) ) 
+			{
+				addFader(this);
+			}
+		});
+	});
+
+
 });
